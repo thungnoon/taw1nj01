8c2e5e2e47af0455ce957103ed191c8ecfe6a7a5
thungnoon <kongzheng.zhou@gmail.com>
2024-11-23 20:59:14 +0800

    Update diy-18.06.sh

--- diy-18.06.sh
+++ diy-18.06.sh
@@ -185,7 +185,7 @@ clone_all https://github.com/linkease/istore luci
 BEGIN_TIME=$(date '+%H:%M:%S')
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
